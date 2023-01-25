045699473159943fbb10bc347aef4572ec80527a
Your Name <luis.martinez33@protonmail.com>
2023-01-25 17:14:07 -0600

    first commit

--- ExchangeRate/app.py
+++ ExchangeRate/app.py
@@ -6,7 +6,7 @@ API_KEY = os.environ["API_KEY"]
 my_url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/USD/MXN"
 
 
-def lambda_handler(event, context):
+def lambdaHandler(event, context):
 
     # r = requests.get(my_url)
     # apiResponse = r.json()
