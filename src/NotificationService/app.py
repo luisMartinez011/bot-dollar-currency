bf9b67b7ced53c49729e2c3281a18dd1156019bb
Your Name <luis.martinez33@protonmail.com>
2023-01-28 00:26:21 -0600

    email notification service works

--- src/NotificationService/app.py
+++ src/NotificationService/app.py
@@ -10,49 +10,50 @@ snsTopicArn = os.environ["SNS_TOPIC_ARN"]
 logger = logging.getLogger(__name__)
 
 
-# snippet-start:[python.example_code.sns.SnsWrapper]
-class SnsWrapper:
-    """Encapsulates Amazon SNS topic and subscription functions."""
+# # snippet-start:[python.example_code.sns.SnsWrapper]
+# class SnsWrapper:
+#     """Encapsulates Amazon SNS topic and subscription functions."""
 
-    def __init__(self, sns_resource):
-        """
-        :param sns_resource: A Boto3 Amazon SNS resource.
-        """
-        self.sns_resource = sns_resource
+#     def __init__(self, sns_resource):
+#         """
+#         :param sns_resource: A Boto3 Amazon SNS resource.
+#         """
+#         self.sns_resource = sns_resource
 
-    def publish_message(topic, message):
-        """
-        Publishes a message, with attributes, to a topic. Subscriptions can be filtered
-        based on message attributes so that a subscription receives messages only
-        when specified attributes are present.
-        :param topic: The topic to publish to.
-        :param message: The message to publish.
-        :param attributes: The key-value attributes to attach to the message. Values
-                            must be either `str` or `bytes`.
-        :return: The ID of the message.
-        """
-        try:
-            response = topic.publish(
-                Message=message,)
-            message_id = response['MessageId']
-            logger.info(
-                "Published message with attributes %s to topic %s.",
-                topic.arn)
-        except ClientError:
-            logger.exception(
-                "Couldn't publish message to topic %s.", topic.arn)
-            raise
-        else:
-            return message_id
+#     def publish_message(topic, message):
+#         """
+#         Publishes a message, with attributes, to a topic. Subscriptions can be filtered
+#         based on message attributes so that a subscription receives messages only
+#         when specified attributes are present.
+#         :param topic: The topic to publish to.
+#         :param message: The message to publish.
+#         :param attributes: The key-value attributes to attach to the message. Values
+#                             must be either `str` or `bytes`.
+#         :return: The ID of the message.
+#         """
+#         try:
+#             response = topic.publish(
+#                 Message=message,)
+#             message_id = response['MessageId']
+#             logger.info(
+#                 "Published message with attributes %s to topic %s.",
+#                 topic.arn)
+#         except ClientError:
+#             logger.exception(
+#                 "Couldn't publish message to topic %s.", topic.arn)
+#             raise
+#         else:
+#             return message_id
 
 
 def lambdaHandler(event, context):
-    # message = {"foo": "bar"}
+    exchangeValue = event["exchangeValue"]
+    message = f"USD dollar is equal to {exchangeValue}"
 
-    # client = boto3.client('sns')
-    # response = client.publish(
-    #     TopicArn=snsTopicArn,
-    #     Message=json.dumps({'default': json.dumps(message)}),
-    #     MessageStructure='json'
-    # )
+    client = boto3.client('sns')
+    response = client.publish(
+        TopicArn=snsTopicArn,
+        Message=json.dumps({'default': json.dumps(message)}),
+        MessageStructure='json'
+    )
     return 4
